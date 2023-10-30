cff9f7f22a2729e92fb21fb528d817baa3c8186c
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-10-30 15:16:36 +0100

    readme and scaffolding

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+netx mi prendo index poi ogni click index ++ faccio un active slide (:src'slide[activeSlide].image) mentre per le immagini laterali ne imposto una vuota e faccio un ciclo for v-for='(slide,index) in slides' e img :src='slide.image e se l'indice è ugale all'indice della foto visualizzata allora avro una classe active sulle immagini a lato
+
+- inizializzo vue
+- 2 variabili in data:
+    - array slides
+    - active slide (index dello slide visualizzato)
+- visualizzare in html active slide
+- v for per le slide
+- implementare funzioni next e prev
+
+bonus
+aggiungere il click al thumbs utilizzando index per farla visualizzare
+*/
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Vue slider
+
+## Description
+
+Starting from the provided markup, implement a carousel with Vue.
+
+### *Bonuses:*
+Clicking a thumb displays the corresponding image large
